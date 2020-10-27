2541bc2b902fe54a79e6d0a0bbd765f8ad1ce6c4
oriHab <orielh@anyvision.co>
2020-10-27 19:21:07 +0200

    hypervisor: refactored hypervisor.py into __init__ and __main__ files
    In addition changed python command of hypevisor.sh file accordingly.

--- resource_managers/hypervisor.sh
+++ resource_managers/hypervisor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 CURRENT_DIR=$(dirname ${BASH_SOURCE[0]})
-PYTHONPATH=$PYTHONPATH:$CURRENT_DIR python3 ${CURRENT_DIR}/hypervisor/hypervisor.py $*
+PYTHONPATH=$PYTHONPATH:$CURRENT_DIR python3 ${CURRENT_DIR}/hypervisor $*
