f424ca44759862b0c5fbfe674604b32697c7afd6
oriHab <orielh@anyvision.co>
2020-11-17 14:41:10 +0200

    vm.py: added kwargs to ctor and json property generic
    We know we at least want to add key_file_path or pem_key_string to vm
    fields, but there are already so many params in the constructor we might
    as well allow anything and just save the param to the vms attributes.
    
    In addition, the vm.json property was hardcoded sending only some of the
    vms fields. So instead of needing to add fields manually, better just
    return all object fields, as we dont know what we may want in the
    future.

--- resource_managers/hypervisor/vms/vm.py
+++ resource_managers/hypervisor/vms/vm.py
@@ -8,7 +8,7 @@ class VM(object):
 
     def __init__(self, name, num_cpus, memsize, sol_port, base_image,
                  base_image_size=None, net_ifaces=None, pcis=None, disks=None, api_version=None,
-                 image=None, cloud_init_iso=None, uuid=None, allocation_id=None, requestor=None):
+                 image=None, cloud_init_iso=None, uuid=None, allocation_id=None, requestor=None, **kwargs):
         self.net_ifaces = net_ifaces or []
         self.pcis = pcis or []
         self.disks = disks or []
@@ -25,24 +25,16 @@ class VM(object):
         self.base_image_size = base_image_size
         self.allocation_id = allocation_id
         self.requestor = requestor
+        for k, v in kwargs.items():
+            setattr(self, k, v)
 
     @property
     def json(self):
-        return {"net_ifaces" : self.net_ifaces,
-                "pcis" : [pci.full_address for pci in  self.pcis],
-                "disks" : self.disks,
-                "name"  : self.name,
-                "num_cpus" : self.num_cpus,
-                "memsize" : self.memsize,
-                "sol_port" : self.sol_port,
-                "base_image" : self.base_image,
-                "api_version" : self.api_version,
-                "image" : self.image,
-                "cloud_init_iso": self.cloud_init_iso,
-                "uuid" : self.uuid,
-                "base_image_size" : self.base_image_size,
-                "allocation_id": self.allocation_id,
-                "requestor": self.requestor}
+        attrs = {
+            k: getattr(self, k) for k in dir(self) if not callable(k) and not k.startswith('__')}
+        pci_vals = attrs.pop('pcis', None)
+        attrs['pcis'] = [pci.full_address for pci in pci_vals] if pci_vals else None
+        return attrs
 
     def __repr__(self):
         data = self.json
