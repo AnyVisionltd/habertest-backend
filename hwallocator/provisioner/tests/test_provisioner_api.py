e2bb06577260e2a609bfa58c0d71c27b4fb286fb
oriHab <orielh@anyvision.co>
2020-12-15 23:20:18 +0200

    test_provisioner_api.py: added external_ip to requestor dict

--- hwallocator/provisioner/tests/test_provisioner_api.py
+++ hwallocator/provisioner/tests/test_provisioner_api.py
@@ -8,6 +8,7 @@ import uuid
 
 import aiohttp
 import pytest
+import requests
 
 from ..webapp import rm_requestor
 
@@ -22,7 +23,7 @@ async def test_resource_manager_requestor_happy_flow():
     """Set ip:port of RESOURCE_MANAGER_EP"""
     rm = dict(endpoint=RESOURCE_MANAGER)
     data = {"demands": {"host": {}},
-            'requestor': dict(hostname=socket.gethostname(), username=getpass.getuser(), ip=socket.gethostbyname(socket.gethostname()))}
+            'requestor': dict(hostname=socket.gethostname(), username=getpass.getuser(), ip=socket.gethostbyname(socket.gethostname()), external_ip=requests.get("http://ifconfig.me").text)}
     possible = await rm_requestor.theoretically_fulfill(rm, data)
     assert possible
     allocate_result = await rm_requestor.allocate(rm['endpoint'], data)
