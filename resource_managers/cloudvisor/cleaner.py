d7451039bbb72e3780047176a272c6863ec52ba5
oriHab <orielh@anyvision.co>
2020-12-22 09:46:56 +0200

    provisioner/expires.py: a lot of bugfixes here
    1. only remove allocations which cant connect to rm after 1 hour, so
       that hopefully we wont lose allocatin between deployments.
    2. refactored to make flow much clearer.

--- resource_managers/cloudvisor/cleaner.py
+++ resource_managers/cloudvisor/cleaner.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 import aiohttp
 
+from cloudvisor.cloud_vm import VM
 from cloudvisor.ec2_wrapper import EC2Wrapper
 
 
@@ -25,24 +26,33 @@ class Cleaner(object):
     async def clean_dangling_vms(self):
         tasks = list()
         active_allocations = await self.active_allocation_ids()
-        vms = await self.ec2_manager.list_active_instances()
-        logging.info(f"cleaner: num active allocations: {len(active_allocations)}; active vms: {len(vms)}")
-        for vm in vms:
+        active_vms = await self.ec2_manager.list_active_vms()
+        logging.info(f"cleaner: num active allocations: {len(active_allocations)}; active vms: {len(active_vms)}")
+        for vm in active_vms:
             if vm.allocation_id not in active_allocations:
                 logging.info(f"found dangling vm: {vm.allocation_id} \n{vm.json}\n")
-                if vm.tags.get("requestor:long_lasting", False):
-                    logging.warning(f"long-lasting instance. reason: {vm.tags['requestor:long_lasting']}.. skipping")
+                if vm.long_lasting:
+                    logging.warning(f"long-lasting instance. reason: {vm.long_lasting()}.. skipping")
                     continue
 
-                if vm.tags['cloudvisor_id'] != self.ec2_manager.ec2_wrapper.id:
-                    if not (EC2Wrapper.main_cloudvisor_id(self.ec2_manager.ec2_wrapper.id) and EC2Wrapper.main_cloudvisor_id(vm.tags['cloudvisor_id'])):
-                        logging.warning(f"vm wasnt created by {self.ec2_manager.ec2_wrapper.id}, "
+                instance_creator = vm.tags['cloudvisor_id']
+                # I want to avoid the situation where 1 cloudvisor creates a machine via 1 provisioner but the cleaner
+                # of a different cloudvisor removes it because it obv doesnt appear in her redis allocations
+                # So if they dont have the same parent and they arent both from main_cloudvisors I want to skip
+                if instance_creator != self.ec2_manager.ec2_wrapper.id and \
+                        EC2Wrapper.main_cloudvisor(self.ec2_manager.ec2_wrapper.id) != EC2Wrapper.main_cloudvisor(instance_creator):
+                    logging.warning(f"vm wasnt created by {self.ec2_manager.ec2_wrapper.id}, "
                                     f"rather {vm.tags['cloudvisor_id']}. skipping")
-                        continue
+                    continue
 
-                logging.warning("destroying...")
+                # Must have the same creator, so lets destroy it.
+                logging.warning(f"adding destroy_instance({vm.name}) to tasks")
                 tasks.append(self.ec2_manager.destroy_instance(vm.name))
+            else:
+                logging.info("instance is an active allocation")
+        logging.info("awaiting gather of destory tasks...")
         await asyncio.gather(*tasks)
+        logging.info("done cleaning dangling vms")
 
     async def clean_dangling_security_groups(self):
         logging.info("cleaning dangling security groups")
