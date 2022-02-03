9fe56e709724fcc167bd3c31145682db4b9bf19d
Eyal Caspi <eyalc@eyalc-MacBook-Pro.local>
2022-02-03 13:02:04 +0200

    ec2_manager | changed gpu instance types

--- resource_managers/cloudvisor/ec2_manager.py
+++ resource_managers/cloudvisor/ec2_manager.py
@@ -6,10 +6,9 @@ import logging
 from cloudvisor.cloud_vm import VM
 from botocore.exceptions import ClientError
 
-INSTANCE_TYPES_BY_GPU_COUNT = {'1': ['g3.4xlarge', 'g4dn.2xlarge', 'g4dn.4xlarge'],
-                               '2': ['g3.8xlarge'],
-                               '4': ['g4dn.12xlarge', 'g3.16xlarge'],
-                               '8': ['p2.8xlarge', 'p3.16xlarge' ]}
+INSTANCE_TYPES_BY_GPU_COUNT = {'1': ['g4dn.2xlarge', 'g4dn.4xlarge'],
+                               '4': ['g4dn.12xlarge'],
+                               '8': ['p3.16xlarge', 'g4dn.metal']}
 
 
 class EC2Manager(object):
